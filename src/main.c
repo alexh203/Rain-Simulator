5bebcac33784e646bdeed12cc71383d4fd547baa
Alexh203 <hawesalex203@protonmail.com>
2025-11-30 11:51:41 +0000

    Displaying window

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
@@ -7,6 +8,20 @@ int main(int argc, char *argv[]){
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Rain sim", 800, 600, SDL_WINDOW_RESIZABLE);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    SDL_Event event;
+    bool running = true;
+
+    while (running){
+        while (SDL_PollEvent(&event)){
+            if(event.type == SDL_EVENT_QUIT)
+                running = false;
+        }
+        SDL_SetRenderDrawColor(renderer,10,10,20,255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+
 
 
     SDL_DestroyRenderer(renderer);
